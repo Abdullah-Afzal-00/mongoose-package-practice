f24ce9374c895d25c8f91ac2fa198207f3a7405b
Abdullah <mabdullahafzal09@gmail.com>
2022-07-19 12:28:59 +0500

    Added file upload Function by multer

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const multer = require("multer");
 require("./config");
 const aCollection = require("./firstCollection");
 
 const app = express();
 
 app.use(express.json());
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    },
+  }),
+}).single("user_file");
+
+app.post("/upload", upload, (req, res, next) => {
+  res.status(200).send("We Good");
+});
 
 app.post("/create", async (req, res, next) => {
   let data = new aCollection(req.body);
@@ -28,6 +44,16 @@ app.delete("/delete/:_id", async (req, res, next) => {
 app.put("/update/:_id", async (req, res, next) => {
   console.log(req.params._id);
   let data = await aCollection.update(req.params, { $set: req.body });
+  res.send(data);
+});
+
+app.get("/search/:key", async (req, res, next) => {
+  let data = await aCollection.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { class: { $regex: req.params.key } },
+    ],
+  });
   res.send(data);
 });
 
